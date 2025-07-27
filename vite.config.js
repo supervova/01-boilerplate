dba099991f6cfaeff923f76e522e14406ea260bb
Vladimir Nikishin <nickysheen@yandex.ru>
2025-07-27 18:04:40 +0200

    feat: Add playground page

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,44 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.scss'),
-      formats: ['es', 'cjs'],
+export default defineConfig(({ command }) => {
+  // Build the library
+  if (command === 'build') {
+    return {
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'src/main.scss'),
+          formats: ['es', 'cjs'],
+        },
+        rollupOptions: {
+          output: { assetFileNames: 'main.css' },
+        },
+      },
+      css: {
+        postcss: {
+          plugins: [require('autoprefixer'), require('postcss-csso')],
+        },
+      },
+    };
+  }
+
+  // 2.  Dev. server: root = example/
+  return {
+    root: path.resolve(__dirname, 'example'),
+    server: {
+      open: true,
+      port: 5173,
     },
-    rollupOptions: {
-      output: { assetFileNames: 'index.css' },
+    resolve: {
+      alias: {
+        // make `import '../src/...'` works from example/
+        '@src': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [require('autoprefixer'), require('postcss-csso')],
+    css: {
+      preprocessorOptions: {
+        scss: { charset: false }, // removes @charset from each HMR injection
+      },
     },
-  },
+  };
 });
